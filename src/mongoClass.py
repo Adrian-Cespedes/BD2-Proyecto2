86823faac3456e141ec7335e382e075a47e785e5
Anlec <103434714+marcelochincha@users.noreply.github.com>
2024-06-15 01:31:31 -0500

    Update mongoClass.py

--- src/mongoClass.py
+++ src/mongoClass.py
@@ -11,13 +11,15 @@ class Mongo:
         if Mongo.col_name not in self.db.list_collection_names():
             records = dataton.reset_index().to_dict(orient='records')
             self.db[Mongo.col_name].insert_many(records)
-            status = self.db[Mongo.col_name].create_index([('text', 'text')],default_language='english')
+            status = self.db[Mongo.col_name].create_index([('text', 'text')])
             print("MongoDB collection created and filled")
             print("Index status: ", status)
 
     def retrieve(self, query, k):
-        result = self.db[Mongo.col_name].find({'$text': {'$search': query}},{'score': {'$meta': 'textScore'}}).sort([('score', {'$meta': 'textScore'})])
-        return [(r['index'], r['score']) for r in result][:k]
+        result = self.db[Mongo.col_name].find({'$text': {'$search': query}},{'score': {'$meta': 'textScore'}})
+        result = list(result)
+        result.sort(key=lambda x: x['score'], reverse=True)
+        return [x['index'] for x in result[:k]]
                                       
 if __name__ == "__main__":
     dataton = pd.read_csv("data/spotify_millsongdata_1000.csv")
